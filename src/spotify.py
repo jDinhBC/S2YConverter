ae954cd9f24e5ac1a45608b7c5eae2de519049af
Jason Dinh <47726701+jDinhBC@users.noreply.github.com>
2023-08-15 01:25:32 -0400

    added function to get user id

--- src/spotify.py
+++ src/spotify.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 import base64
 from requests import post
+import re
 
 load_dotenv()
 
@@ -12,19 +13,42 @@ client_secret = os.getenv("CLIENT_SECRET")
 def get_token():
     auth_string = client_id + ":" + client_secret
     auth_bytes = auth_string.encode("utf-8")
-    auth_base64 = str(base64.b64encode(auth_base64), "utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
 
     url = "https://accounts.spotify.com/api/token"
-    _headers = {
-        "Authorization": "Basic" + auth_base64,
+    headers = {
+        "Authorization": "Basic " + auth_base64,
         "Content-Type": "application/x-www-form-urlencoded"
     }
     data = {"grant_type": "client_credentials"}
-    result = post(url, headers=_headers, data=data)
+    result = post(url, headers=headers, data=data)
     json_result = json.loads(result.content)
+    token = None
+    if ("access_token" not in json_result):
+        token = "Unable to retrieve access token"
     token = json_result["access_token"]
     return token
 
+def get_auth_header(token):
+    return {"Authorization": "Bearer "+ token}
+
+def get_user_id(url):
+    # takes url of spotify user profile and extracts id
+    url = input("Enter spotify profile url: ")
+    pattern = r'https://open\.spotify\.com/user/([A-Za-z0-9]+)'
+    checkUrl = re.fullmatch(pattern, url)
+
+    if not checkUrl:
+        return "Url does not fit the criteria"
+    
+    matches = re.search(pattern, url)
+
+    if not matches:
+        return "User ID not found"
+    
+    user_id = matches.group(1)
+    return user_id
+
+
 
 token = get_token()
-print(token)
